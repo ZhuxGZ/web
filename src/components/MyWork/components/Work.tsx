d1e8753f175891fc68f17bbba10dca22822de289
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-12-24 16:58:57 -0300

    feat(Techlist): add the MyWork component

---
+++ src/components/MyWork/components/Work.tsx
@@ -0,0 +1,36 @@
+import { TechList } from '../..';
+import { Techs } from '../../TechList/TechList';
+import styles from './Work.module.scss';
+
+interface WorkProps {
+	description: string;
+	role: string;
+	company: string;
+	date: string;
+	techs: Techs[];
+	tasklist: string[];
+}
+
+export const Work = ({ description, role, company, date, techs, tasklist }: WorkProps) => {
+	return (
+		<section>
+			<h3 className={styles.title}>
+				{role} <span className={styles.dot}>·</span> {company}
+			</h3>
+			<p className={styles.date}>{date}</p>
+			<p>{description}</p>
+			<ul className={styles.list}>
+				{tasklist?.map((task) => {
+					return (
+						<li key={task}>
+							<span>{task}</span>
+						</li>
+					);
+				})}
+			</ul>
+			<section className={styles.tech_list}>
+				<TechList techs={techs} />
+			</section>
+		</section>
+	);
+};
