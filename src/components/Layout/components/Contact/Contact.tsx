64284f85612d27708eb248b6675503d64728f082
Ignacio Bergantinos <bergantinos.ignacio@gmail.com>
2024-10-20 22:51:44 -0300

    feat(contact): change email direction

--- src/components/Layout/components/Contact/Contact.tsx
+++ src/components/Layout/components/Contact/Contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
 			<h2>Contact Me</h2>
 			<p>
 				If you have an offer in mind feel free to reach to{' '}
-				<Link href="mailto:bergantinos.ignacio@gmail.com">bergantinos.ignacio@gmail.com</Link>
+				<Link href="mailto:hello@zhux.dev">hello@zhux.dev</Link>
 			</p>
 		</section>
 	);
